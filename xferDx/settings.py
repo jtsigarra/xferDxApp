ca5ace3d0d6264e8e2ef3413af1831ed54c1bf18
unknown <jtsigarra@up.edu.ph>
2025-12-14 17:58:00 +0800

    change

--- xferDx/settings.py
+++ xferDx/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'xferDx.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_PUBLIC_URL"),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
