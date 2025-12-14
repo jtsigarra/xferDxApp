ff17264fe14b826f125a595c2109c4e330ba4641
unknown <jtsigarra@up.edu.ph>
2025-12-14 17:43:49 +0800

    change

--- xferDx/settings.py
+++ xferDx/settings.py
@@ -76,16 +76,12 @@ WSGI_APPLICATION = 'xferDx.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("PGDATABASE"),
-        "USER": os.environ.get("PGUSER"),
-        "PASSWORD": os.environ.get("PGPASSWORD"),
-        "HOST": os.environ.get("PGHOST"),
-        "PORT": os.environ.get("PGPORT", 5432),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_PUBLIC_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
