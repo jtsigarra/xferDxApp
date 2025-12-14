62df531277c20561c27114dd9793894e9d2ddabc
unknown <jtsigarra@up.edu.ph>
2025-12-15 00:10:00 +0800

    email

--- xferDx/settings.py
+++ xferDx/settings.py
@@ -136,7 +136,7 @@ LOGIN_REDIRECT_URL = '/dashboard/'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
